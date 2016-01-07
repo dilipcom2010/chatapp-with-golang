2f34c2d67b670fe6fa5cdb94e114dd1f80f840cb
Dilip Kumar <dilip@machinehappy.com>
2016-01-07 13:04:44 +0530

    new commit

--- html_test/test3.go
+++ html_test/test3.go
@@ -25,17 +25,19 @@ type connsList []*connection
 type connsDict map[string] connsList
 type strList [] string
 
+
 type connection struct {
 	ws *websocket.Conn
 	send chan []byte
 }
+
 
 type connStrt struct{
 	id string
 	connection *connection
 }
 
-
+/*  */
 type hub struct{
 	conns connsDict
 	register chan connStrt
@@ -44,10 +46,10 @@ type hub struct{
 }
 
 var h = hub{
-	conns: make(connsDict),
-	register: make(chan connStrt),
-	unregister: make(chan connStrt),
-	broadcast:   make(chan []byte),
+	conns: make(connsDict), /* stores all the sockets of an user */
+	register: make(chan connStrt), /* Whenever a new socket is opened it is sent to register via this channel */
+	unregister: make(chan connStrt), /* Whenever a socket quits, it is sent to unregister via this channel */
+	broadcast:   make(chan []byte), /* All the messages that need broadcast send to this channel to broadcast */
 }
 
 //var conns = make(map[string]connection)
@@ -55,12 +57,31 @@ var db *sql.DB
 var store = sessions.NewCookieStore([]byte("chatapp"))
 
 
+
+
 func (h *hub)run() {
+	/* 
+		This is a goroutine. Runs forever.
+		Responsibility :: 
+			Register a new connection
+				When an user opens a new tab, a new connection is created.
+				Tis connection is send to register WRT that user via register channel.
+				This forever loop accepts the connection from the register channel and stores it in WRT the that user id.
+			Unregister a connection
+				When an user closes a tab then the connection(wrapper of socket and message sending channel) is disconnected.
+				So the connection needs to remove.
+				This forever loop accepts that disconnected channel and removes it from user id previously it is registered.
+			broadcast a message
+				It fetches the destination user id from the message.
+				gets all the connection of that user and writes the message into their sending channel.
+
+			It also calls a goroutine to write the message into databese.
+	*/
 	for{
 		select{
 		case c := <-h.register:
 			//h.conns[c.id] = c.connection
-			h.conns[c.id] = append(h.conns[c.id], c.connection)
+			h.conns[c.id] = append(h.conns[c.id], c.connection) /* c.id == id of the user who has opened the new tab */
 		
 		case c := <-h.unregister:
 			fmt.Println("deleting connection......")
@@ -98,13 +119,15 @@ func (h *hub)run() {
 }
 
 
+
+
 func main() {
 	connectdb()
 	go h.run()
 	//go users.startnotify()
 	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, r.URL.Path[1:])
-    })
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
 	http.HandleFunc("/", root)
 	http.HandleFunc("/room", room)
 	http.HandleFunc("/chat", chat)
@@ -122,25 +145,44 @@ func main() {
 
 
 
+
 func connectdb() {
-	db, _ = sql.Open("mysql", "root:dkumar@tcp(localhost:3306)/chatapp")
+	/* connects to the database chatapp with username root and  password fyogi */
+	db, _ = sql.Open("mysql", "root:fyogi@tcp(localhost:3306)/chatapp")
 }
 
+
+
+
 func (c *connection) reader() {
+	/* 
+		When the user opens a new tab a new socket has created, then this function starts reading messages from the socket.
+		And if it get any message it writes it to broadcast channel to broadcast the message.
+	*/
 	for {
-		_, message, err := c.ws.ReadMessage()
+		_, message, err := c.ws.ReadMessage() /* reading from socket */
 		fmt.Println("success")
 		if err != nil {
+			//it the tab gets closed. then the above ReadMessage() function will not work
 			break
 		}
 		h.broadcast <- message
 		//do something..........
 	}
-	c.ws.Close()
+	c.ws.Close() /* the above loop will break down once the user closes the tab. Then we need to close the socket */
 	fmt.Println("reader existing.....")
 }
 
+
+
+
 func (c *connection) writer() {
+	/*
+		When a user opens a new tab, a new connection is created wrapping two things, the newly created socket and a channel named as send.
+		when other connections needs to send message into this tab then they will write message into its send channel.
+		here the message will be recieved from this send channel and write it into it's socket.
+		the way message is notified on front end.
+	*/
 	for message := range c.send {
 		fmt.Println("message====", string(message))
 		err := c.ws.WriteMessage(websocket.TextMessage, message)
@@ -154,13 +196,17 @@ func (c *connection) writer() {
 
 
 
+
 func root(w http.ResponseWriter, r *http.Request) {
 	t,_ := template.ParseFiles("base.html")
 	t.Execute(w, nil)
 }
 
 
+
+
 func update_dp(filename string, id int) {
+	/* Updating profile picture of user */
 	stmtUpd, err := db.Prepare("update users set selfie=? where id=?")
 	if err != nil {
 		fmt.Println("unable to prepare updation statement")
@@ -168,12 +214,17 @@ func update_dp(filename string, id int) {
 	//defer stmtUpd.close()
 
 	_, err = stmtUpd.Exec(filename, id)
-    if err != nil {
-        fmt.Println("unable to update into db")
-    }
+	if err != nil {
+		fmt.Println("unable to update into db")
+	}
 
 }
+
+
+
+
 func validate_dp(buff []byte) bool {
+	/* Responsible to validate uploaded file */
 	//buff := make([]byte, 512) // why 512 bytes ? see http://golang.org/pkg/net/http/#DetectContentType
 	//_, err = file.Read(buff)
 
@@ -200,50 +251,60 @@ func validate_dp(buff []byte) bool {
 		}
 }
 
+
+
+
 func room(w http.ResponseWriter, r *http.Request) {
+	/*
+		When a user inters into chat lobby then this function is called.
+		It return an html page that is responsible for the UI of chat lobby.
+		If a user uploads profile picture then it is also handled by this function.
+		Rteurn chal lobby html page if method is get.
+		handle profile picture if method is post.
+	*/
 	session,_ := store.Get(r, "active")
 	if r.Method == "POST" && session.Values["login"]==true{
 		file, header, err := r.FormFile("profile-pic")
 		if err != nil {
-                 fmt.Fprintln(w, err)
-                 return
-         }
+				 fmt.Fprintln(w, err)
+				 return
+		 }
 
-        /*var file1 = file
-        buff := make([]byte, 512) 
-        _, err = file1.Read(buff)
-        if err != nil {
-        	fmt.Println(err)
-        	os.Exit(1)
-        }
-        
-        if validate_dp(buff) != true {
-        	fmt.Fprint(w, "please use only jpg/png/gif file format")
-        	return
-        }*/
-        //fmt.Println(file1, header.Filename)
-        u, _ := uuid.NewV4()
-        extn := filepath.Ext(header.Filename)
-        if extn != ".jpg" && extn != ".png" && extn != ".gif" && extn != ".jpeg"{
-        	fmt.Fprint(w, "please use only jpg/png/gif file format")
-        	return
-        }
-        filename := "static/images/profile-pic/"+u.String()+extn
-        out, err := os.Create(filename)
-        if err != nil {
-                fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
-                return
-        }
-        //out := "/static/images/profile-pic/"
-        _, err = io.Copy(out, file)
-        if err != nil {
-                fmt.Fprintln(w, err)
-        }
-        usrid,_ := strconv.Atoi(session.Values["id"].(string))
-        filename = "/" + filename
-        session.Values["dp"] = filename
-        session.Save(r, w)
-        update_dp(filename, usrid)
+		/*var file1 = file
+		buff := make([]byte, 512) 
+		_, err = file1.Read(buff)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		
+		if validate_dp(buff) != true {
+			fmt.Fprint(w, "please use only jpg/png/gif file format")
+			return
+		}*/
+		//fmt.Println(file1, header.Filename)
+		u, _ := uuid.NewV4()
+		extn := filepath.Ext(header.Filename)
+		if extn != ".jpg" && extn != ".png" && extn != ".gif" && extn != ".jpeg"{
+			fmt.Fprint(w, "please use only jpg/png/gif file format")
+			return
+		}
+		filename := "static/images/profile-pic/"+u.String()+extn
+		out, err := os.Create(filename)
+		if err != nil {
+				fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
+				return
+		}
+		//out := "/static/images/profile-pic/"
+		_, err = io.Copy(out, file)
+		if err != nil {
+				fmt.Fprintln(w, err)
+		}
+		usrid,_ := strconv.Atoi(session.Values["id"].(string))
+		filename = "/" + filename
+		session.Values["dp"] = filename
+		session.Save(r, w)
+		update_dp(filename, usrid)
 	}
 
 
@@ -270,12 +331,30 @@ func room(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(reflect.TypeOf(x))
 	
 	t, _ := template.ParseFiles("home1.html")
-    t.Execute(w, data)
+	t.Execute(w, data)
 }
 
 
+
+
 func chat(w http.ResponseWriter, r *http.Request) {
-	session,_ := store.Get(r, "active")
+	/*
+		This is the function called by websocket.
+		if a user hits /chat url then this function is called and websocket is created.
+		Functionality:
+			Starts websocket
+			wraps sending channel and socket into a connection
+			writes this connection into register channel
+			starts a goroutine writer
+			starts a goroutine for fetching all unseen messages
+			starts a reader...it is forever loop
+			when the socket gets close then also reder and writer exist. after that it writes the connection into unregister channel
+
+	*/
+	session,err := store.Get(r, "active")
+	if err != nil{
+		http.Redirect(w,r, "/login", 302)
+	}
 	
 	fmt.Println("regestring connection...")
 	var upgrader = &websocket.Upgrader{ReadBufferSize:1024, WriteBufferSize:1024}
@@ -294,7 +373,14 @@ func chat(w http.ResponseWriter, r *http.Request) {
 	
 }
 
+
+
+
 func get_unseen_msgs(t string) {
+	/* 
+		Gets all messages from the database whose status is unseen.
+		Write these messages into broadcast channel.
+	 */
 	id,_ := strconv.Atoi(t)
 	stmtOut, err := db.Prepare("select id, message from chats where usrTo=? and seen=false")
 	if err != nil {
@@ -303,10 +389,10 @@ func get_unseen_msgs(t string) {
 	defer stmtOut.Close()
 
 	rows, err := stmtOut.Query(id)
-    if err != nil {
-        fmt.Println("Error in selecting from db")
-    }
-    defer rows.Close()
+	if err != nil {
+		fmt.Println("Error in selecting from db")
+	}
+	defer rows.Close()
 
 	msgs := decode(rows)
 	for _,m := range msgs {
@@ -316,7 +402,13 @@ func get_unseen_msgs(t string) {
 	//fmt.Println(msgs)
 }
 
+
+
+
 func dump_msg(to string, from string, m []byte, seen bool) {
+	/*
+		Responsible for writing messages into database
+	*/
 	usrTo,_ := strconv.Atoi(to)
 	//fmt.Println(from)
 	usrFrom,_ := strconv.Atoi(from)
@@ -330,12 +422,19 @@ func dump_msg(to string, from string, m []byte, seen bool) {
 	defer stmtIns.Close()
 
 	_, err = stmtIns.Exec(usrFrom, usrTo, message, seen)
-    if err != nil {
-        fmt.Println("unable to insert into db")
-    }
+	if err != nil {
+		fmt.Println("unable to insert into db")
+	}
 }
 
+
+
+
 func update_msg(t string) {
+	/*
+		It gets the id of message and meake it seen from unseen.
+		This happens when the user gets online and seen unseen messages.
+	*/
 	id,_ := strconv.Atoi(t)
 	fmt.Println(id)
 	stmtUpd, err := db.Prepare("update chats set seen=true where id=?")
@@ -345,12 +444,18 @@ func update_msg(t string) {
 	//defer stmtUpd.close()
 
 	_, err = stmtUpd.Exec(id)
-    if err != nil {
-        fmt.Println("unable to update into db")
-    }
+	if err != nil {
+		fmt.Println("unable to update into db")
+	}
 }
 
+
+
+
 func get_prev_talk(w http.ResponseWriter, r *http.Request) {
+	/*
+		It fetches all unseen messages.
+	*/
 	session,_ := store.Get(r, "active")
 	if session.Values["login"] !=true {
 		http.Redirect(w,r, "/login", 302)
@@ -369,10 +474,10 @@ func get_prev_talk(w http.ResponseWriter, r *http.Request) {
 	defer stmtOut.Close()
 
 	rows, err := stmtOut.Query(u1, u2, u2, u1, offset, limit)
-    if err != nil {
-        fmt.Println("Error in selecting from db")
-    }
-    defer rows.Close()
+	if err != nil {
+		fmt.Println("Error in selecting from db")
+	}
+	defer rows.Close()
 
 	msgs := decode(rows)
 
@@ -402,9 +507,6 @@ func get_prev_talk(w http.ResponseWriter, r *http.Request) {
 
 
 
-
-
-
 func signup(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		session,_ := store.Get(r, "active")
@@ -427,6 +529,8 @@ func signup(w http.ResponseWriter, r *http.Request) {
 	}
 	
 }
+
+
 
 
 func login(w http.ResponseWriter, r *http.Request) {
@@ -475,6 +579,8 @@ func login(w http.ResponseWriter, r *http.Request) {
 }
 
 
+
+
 func logout(w http.ResponseWriter, r *http.Request) {
 	session,_ := store.Get(r, "active")
 	session.Options = &sessions.Options{MaxAge: -1}
@@ -483,47 +589,49 @@ func logout(w http.ResponseWriter, r *http.Request) {
 }
 
 
+
+
 func decode(rows *sql.Rows) ([]dict){
 
 	columns, err := rows.Columns()
-    if err != nil {
-        panic(err.Error()) // proper error handling instead of panic in your app
-    }
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
 
-    values := make([]sql.RawBytes, len(columns))
-    var data []dict
+	values := make([]sql.RawBytes, len(columns))
+	var data []dict
 
 
-    scanArgs := make([]interface{}, len(values))
-    for i := range values {
-        scanArgs[i] = &values[i]
-    }
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
 
-    row_no := 0
-    for rows.Next() {
-        err = rows.Scan(scanArgs...)
-        if err != nil {
-            panic(err.Error()) // proper error handling instead of panic in your app
-        }
+	row_no := 0
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
 
-        var value string
-        row := make(dict)
-        for i, col := range values {
-            if col == nil {
-                value = "NULL"
-            } else {
-                value = string(col)
-            }
-            row[columns[i]] = value
-            //fmt.Println(columns[i], ": ", value)
-        }
-        data = append(data, row)
-    	row_no = row_no + 1
-    }
+		var value string
+		row := make(dict)
+		for i, col := range values {
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			row[columns[i]] = value
+			//fmt.Println(columns[i], ": ", value)
+		}
+		data = append(data, row)
+		row_no = row_no + 1
+	}
 
-    //fmt.Println(data)
-    if err = rows.Err(); err != nil {
-        panic(err.Error()) // proper error handling instead of panic in your app
-    }
-    return data
+	//fmt.Println(data)
+	if err = rows.Err(); err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	return data
 }
